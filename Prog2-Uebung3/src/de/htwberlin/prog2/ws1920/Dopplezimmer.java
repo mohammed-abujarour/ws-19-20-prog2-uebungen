026ddb1dcce68ba2b30184d5b74a055f33a60727
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-11-10 20:54:41 +0100

    modify Dopplezimmer to make it bookable

--- Prog2-Uebung3/src/de/htwberlin/prog2/ws1920/Dopplezimmer.java
+++ Prog2-Uebung3/src/de/htwberlin/prog2/ws1920/Dopplezimmer.java
@@ -1,6 +1,6 @@
 package de.htwberlin.prog2.ws1920;
 
-public class Dopplezimmer extends Zimmer {
+public class Dopplezimmer extends Zimmer implements IBuchbar {
 
 	private boolean seperatedBeds ;
 	
@@ -19,6 +19,12 @@ public class Dopplezimmer extends Zimmer {
 	@Override
 	public String toString() {
 		return  super.toString() + " seperatedBeds= " + seperatedBeds ;
+	}
+
+	@Override
+	public boolean buchen(int day) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 	
