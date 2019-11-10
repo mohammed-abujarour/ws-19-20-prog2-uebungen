76d8e0d6454d25318ec9262db0762be29b9f8bbc
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-11-10 20:54:19 +0100

    Modify Einzelzimmer to make it bookable

--- Prog2-Uebung3/src/de/htwberlin/prog2/ws1920/Einzelzimmer.java
+++ Prog2-Uebung3/src/de/htwberlin/prog2/ws1920/Einzelzimmer.java
@@ -1,10 +1,24 @@
 package de.htwberlin.prog2.ws1920;
 
-public class Einzelzimmer extends Zimmer {
+public class Einzelzimmer extends Zimmer implements IBuchbar {
+	private boolean[] bookings = new boolean[365];
 
 	public Einzelzimmer(String number) {
 		super(number);
 		super.setCapacity(1);
+	}
+
+	@Override
+	public boolean buchen(int day) {
+		
+		if(bookings[day] == true) {
+			return false;
+		}
+		else {
+			bookings[day] = true;
+			return true;
+		}
+	
 	}
 
 }
