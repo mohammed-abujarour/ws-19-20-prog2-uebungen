f984347555e0e0d73ae15f1aa227898ecc963204
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2020-01-07 10:56:34 +0100

    KeyListener

--- GUI1/src/GUIStarter.java
+++ GUI1/src/GUIStarter.java
@@ -7,6 +7,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -22,8 +24,10 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
-class GrundgeruestSwing extends JFrame implements ActionListener, MouseListener, MouseMotionListener {
+class GrundgeruestSwing extends JFrame implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
 
     private JButton klickMichbutton = new JButton("Klick mich"); // Button erzeugen
     private JButton endeButton = new JButton("Ende"); // Button erzeugen
@@ -34,6 +38,9 @@ class GrundgeruestSwing extends JFrame implements ActionListener, MouseListener,
     private JMenuItem openFile = new JMenuItem("Open ... ");
     private JMenuItem saveFile = new JMenuItem("Save");
     private JLabel coordinates = new JLabel();
+
+    private JLabel lblName = new JLabel("Name: ");
+    private JTextField txtName = new JTextField(20);
 
     public GrundgeruestSwing() {
         super(); // Konstruktor von JFrame
@@ -85,6 +92,13 @@ class GrundgeruestSwing extends JFrame implements ActionListener, MouseListener,
         panel.add(label1); // Label ans Panel
         panel.add(klickMichbutton); // Button ans Panel
         panel.add(endeButton);
+
+        panel.add(new JSeparator());
+
+        panel.add(lblName);
+        panel.add(new JSeparator());
+        panel.add(txtName);
+        txtName.addKeyListener(this);
         return panel;
     }
 
@@ -213,12 +227,38 @@ class GrundgeruestSwing extends JFrame implements ActionListener, MouseListener,
 
     }
 
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.CHAR_UNDEFINED)
+            return;
+
+        boolean validInput = key != KeyEvent.VK_0 && key != KeyEvent.VK_1 && key != KeyEvent.VK_2
+                && key != KeyEvent.VK_3 && key != KeyEvent.VK_4 && key != KeyEvent.VK_5 && key != KeyEvent.VK_6
+                && key != KeyEvent.VK_7 && key != KeyEvent.VK_8 && key != KeyEvent.VK_9;
+        if (!validInput) {
+            JOptionPane.showMessageDialog(this, "Invalider Input! Ein Name darf keine Zahlen beinhalten");
+            txtName.setText(txtName.getText().substring(0, txtName.getText().length() - 1));
+            return;
+        }
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
 
 public class GUIStarter {
     public static void main(String args[]) {
         GrundgeruestSwing hauptfenster = new GrundgeruestSwing();
-        hauptfenster.setSize(400, 300); // wie groß?
+        hauptfenster.setSize(320, 250); // wie groß?
         hauptfenster.setLocation(200, 300); // wo ?
         hauptfenster.setVisible(true); // sichtbar
     }
