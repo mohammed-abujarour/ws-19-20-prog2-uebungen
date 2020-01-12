1af322f8a04684a9684f7acd13480d0362f6b9f1
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2020-01-12 22:25:25 +0100

    Projektvorlage für Übung 10

---
+++ Prog2-Uebung10/src/com/awesomeui/hotel/Starter.java
@@ -0,0 +1,20 @@
+package com.awesomeui.hotel;
+
+import de.htwberlin.prog2.ws1920.Backend;
+import de.htwberlin.prog2.ws1920.Hotel;
+
+/**
+ * @author Mohammed AbuJarour (mohammed.abujarour@htw-berlin.de)
+ *
+ */
+public class Starter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Hotel hotel = Backend.getHotel();
+        // Frame etc.
+    }
+
+}
